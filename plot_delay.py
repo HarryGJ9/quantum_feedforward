17f3515f4314c469cd1f61c58dbc1fbee11ddba7
Harry Jones <harry4gjones@gmail.com>
2024-06-27 16:21:18 +0100

    Produces single plot of CH1 and CH2.

--- plot_delay.py
+++ plot_delay.py
@@ -1,2 +1,33 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import os
+
+# Specify OneDrive - UOB path
+one_drive_path = r'C:\Users\harry\OneDrive - University of Bristol\Documents\QE CDT\Project B - Quantum Feedforward Loops\Results\Delay Results'
+
+# Function to read CSV file generated from oscilloscope data
+def read_csv(file_path):
+    data = np.genfromtxt(file_path, delimiter=',', usecols=(3, 4))
+    time = data[:, 0]
+    voltage = data[:, 1]
+
+    return time, voltage
+
+# Specify file paths
+file1 = os.path.join(one_drive_path, 'ALL0000', 'F0000CH1.CSV')
+file2 = os.path.join(one_drive_path, 'ALL0000', 'F0000CH2.CSV')
+
+# Read data from CSV files
+time1, voltage1 = read_csv(file1)
+time2, voltage2 = read_csv(file2)
+
+# Plot data
+plt.figure(figsize=(10,6))
+plt.plot(time1, voltage1, label='Channel 1')
+plt.plot(time2, voltage2, label='Channel 2')
+plt.xlabel('Time (ns)')
+plt.ylabel('Voltage ()')
+plt.legend()
+plt.grid(True)
+plt.show()
+
