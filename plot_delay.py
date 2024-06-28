f9c067fb24c25820288dab92da54837feaf290de
Harry jones <harry.jones@bristol.ac.uk>
2024-06-28 10:44:17 +0100

    Robust plotting of oscilloscope data.

--- plot_delay.py
+++ plot_delay.py
@@ -2,8 +2,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 import os
 
-# Specify OneDrive - UOB path
-one_drive_path = r'C:\Users\harry\OneDrive - University of Bristol\Documents\QE CDT\Project B - Quantum Feedforward Loops\Results\Delay Results'
 
 # Function to read CSV file generated from oscilloscope data
 def read_csv(file_path):
@@ -13,9 +11,36 @@ def read_csv(file_path):
 
     return time, voltage
 
+# Function to find OneDrive path on either home or office PC
+def find_onedrive_path():
+    # Get user's home directory
+    home_dir = os.path.expanduser("~")
+
+    # Specify OneDrive folder name
+    one_drive_name = 'OneDrive - University of Bristol'
+
+    # Construct path to onedrive folder
+    one_drive_path = os.path.join(home_dir, one_drive_name)
+
+    # Check if it exists
+    if os.path.exists(one_drive_path):
+        return one_drive_path
+    else:
+        raise FileNotFoundError("University OneDrive not found.")
+
+# Find University of Bristol's OneDrive path
+try:
+    one_drive_path = find_onedrive_path()
+except FileNotFoundError as e:
+    print(e)
+    exit()
+
+# Specify relative path from onedrive folder
+relative_path = 'Documents/QE CDT/Project B - Quantum Feedforward Loops/Results/Delay Results'
+
 # Specify file paths
-file1 = os.path.join(one_drive_path, 'ALL0000', 'F0000CH1.CSV')
-file2 = os.path.join(one_drive_path, 'ALL0000', 'F0000CH2.CSV')
+file1 = os.path.join(one_drive_path, relative_path, 'ALL0000', 'F0000CH1.CSV')
+file2 = os.path.join(one_drive_path, relative_path, 'ALL0000', 'F0000CH2.CSV')
 
 # Read data from CSV files
 time1, voltage1 = read_csv(file1)
